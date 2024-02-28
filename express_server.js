3ba22017eb96573baa268511bfb3b8ab6272b039
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-27 20:41:45 -0500

    Add logic for urls routing

--- express_server.js
+++ express_server.js
@@ -14,10 +14,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get('./urls', (req, res) => {
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.send('urls_index', templateVars);
-})
+  res.render("urls_index", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);

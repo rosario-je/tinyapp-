2901a3035357b3e970270748ce15888c27f3db57
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-27 22:13:21 -0500

    Add POST logic for submission form

--- express_server.js
+++ express_server.js
@@ -5,25 +5,43 @@ const PORT = 8080;
 //Set templating engine to EJS
 app.set('view engine', 'ejs');
 
+function generateRandomString() {}
+
+
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//Route for homepage
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//Route for urls main page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+//Route for new urls
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+//Route for specific url 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id
   const templateVars = { id: id, longURL: urlDatabase[id]};
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);

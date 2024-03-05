e4c940ede66e6d861fb7c820a3bfa0792794ce27
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-03-04 23:55:41 -0500

    Add new folder with constant functions and database to modularize code

---
+++ modules/lib.js
@@ -0,0 +1,60 @@
+
+/* -----------------Temporary user database-----------------*/
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+/* -----------------Temporary URL database-----------------*/
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+//ID Generator function
+function generateRandomString() {
+  const id = Math.random().toString(36).substring(2,8);
+  return id;
+}
+
+//Function to get user by email from the temporary user database
+const getUserByEmail = (email) => {
+  for (const userId in users) {
+    if (users[userId].email === email) {
+      return users[userId];
+    }
+  }
+  return null;
+};
+
+//Function to check if a user exists;
+const checkForUser = (user) => {
+  if (user){
+    email = user.email
+
+  } else {
+    return false;
+  }
+}
+
+module.exports = { 
+  generateRandomString, 
+  getUserByEmail, 
+  checkForUser, 
+  users, 
+  urlDatabase
+}

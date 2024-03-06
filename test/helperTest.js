9b56f0bcaa0ce7fd94f7614d346b2f94fba82a36
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-03-05 20:31:01 -0500

    Add two test cases

--- test/helperTest.js
+++ test/helperTest.js
@@ -16,9 +16,26 @@ const testUsers = {
 };
 
 describe('The function getUserByEmail:', function() {
+
   it(' - Should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedUserID = "userRandomID";
     // Write your assert statement here
+    assert(testUsers[expectedUserID].email === user.email, 'User exists');
+  });
+
+  it(' - Should return a user with a valid password email', function() {
+    const user = getUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    // Write your assert statement here
+    assert(testUsers[expectedUserID].password === user.password, 'User exists');
+  });
+
+  it(' - Should thrown an error with a message if the user does not exists ', function() {
+    const user = getUserByEmail("user3@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    // Write your assert statement here
+    assert(testUsers[expectedUserID] !== user, 'User does not exists');
   });
 });
+

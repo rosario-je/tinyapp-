164d8137102691c3232a41abd173124ec313722a
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-03-05 20:56:15 -0500

    Refactor test cases

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -20,22 +20,21 @@ describe('The function getUserByEmail:', function() {
   it(' - Should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedUserID = "userRandomID";
-    // Write your assert statement here
+
     assert(testUsers[expectedUserID].email === user.email, 'User exists');
   });
 
-  it(' - Should return a user with a valid password email', function() {
+  it(' - Should return a user with a valid password', function() {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedUserID = "userRandomID";
-    // Write your assert statement here
+
     assert(testUsers[expectedUserID].password === user.password, 'User exists');
   });
 
   it(' - Should thrown an error with a message if the user does not exists ', function() {
-    const user = getUserByEmail("user3@example.com", testUsers)
-    const expectedUserID = "userRandomID";
-    // Write your assert statement here
-    assert(testUsers[expectedUserID] !== user, 'User does not exists');
+    const user = getUserByEmail("unexpectedUser@example.com", testUsers)
+    //const expectedUserID = "userRandomID";
+    assert(user !== false, 'User does not exists');
   });
 });
 

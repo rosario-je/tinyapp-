07f4d07979dce59166e64fa08a0b9fc8a19a140c
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-03-05 21:03:12 -0500

    Add data base

---
+++ dataBase.js
@@ -0,0 +1,27 @@
+/* -----------------Temporary user database-----------------*/
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+/* -----------------Temporary URL database-----------------*/
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+module.exports = { users, urlDatabase }
